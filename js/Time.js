e2bdce4ca31c679a2f25c1cc5df4628fcb2ccace
MudasirBhat <118426882+Mudasir472@users.noreply.github.com>
2023-12-29 00:31:55 +0530

    Add files via upload

---
+++ js/Time.js
@@ -0,0 +1,46 @@
+
+
+var timeContainer=document.querySelectorAll('.con p');
+
+var countDownDate = new Date("Jan 5, 2025 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
+  timeContainer[0].innerHTML=days;
+  timeContainer[1].innerHTML=hours;
+  timeContainer[2].innerHTML=minutes;
+  timeContainer[3].innerHTML=seconds;
+
+
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+
+
+// ----------------for view cart----------------------
+var boxx=document.querySelector('.boxx');
+var bodyTr=document.querySelectorAll('tbody tr'); 
+bodyTr.forEach(function(ele,index){
+  
+});
